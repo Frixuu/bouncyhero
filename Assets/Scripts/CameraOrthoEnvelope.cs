c47b77a070e3118b4c2377ba372231322df0361c
Frixu <frixu@frixu.me>
2019-03-23 21:40:50 +0100

    Make camera's orthographic size fixed to width
    By default, Unity's ortho camera can have a size assigned, but it matches the height instead of the width.
    This might be optimal for some games, like platformers, but is useless to us.
    We're trying to do things our own way.

---
+++ Assets/Scripts/CameraOrthoEnvelope.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Frixu.BouncyHero.Scripts
+{
+    /// <summary> Resizes the camera's size to envelope width instead of height. </summary>
+    [RequireComponent(typeof(Camera))]
+    public class CameraOrthoEnvelope : MonoBehaviour
+    {
+        [Tooltip("Horizontal Size")] public float Size;
+        private new Camera camera;
+
+        private void Start()
+        {
+            camera = GetComponent<Camera>();
+        }
+
+        private void FixedUpdate()
+        {
+            var screenRatio = (float)Screen.width / (float)Screen.height;
+            camera.orthographicSize = Size / screenRatio;
+        }
+    }
+}
