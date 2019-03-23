71fd2af0a36a3199fd09dbafd6e42210f764f6d6
Frixu <frixu@frixu.me>
2019-03-23 23:41:17 +0100

    Make background image fit the screen

---
+++ Assets/Scripts/RectScaler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Frixu.BouncyHero.Scripts
+{
+    [RequireComponent(typeof(Transform))]
+    public class RectScaler : MonoBehaviour
+    {
+        private new Transform transform;
+        public float Size = 10;
+        public float AspectRatio = 2f;
+
+        private void Start()
+        {
+            transform = GetComponent<Transform>();
+        }
+
+        private void FixedUpdate()
+        {
+            var screenRatio = (float)Screen.width / (float)Screen.height;
+            var side = Size;
+            if (screenRatio < AspectRatio) side *= AspectRatio / screenRatio;
+            transform.localScale = new Vector3(side, side, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/SpriteMaskAnimator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Frixu.BouncyHero.Scripts
+{
+    [RequireComponent(typeof(PlayableDirector))]
+    [RequireComponent(typeof(SpriteMask))]
+    public class SpriteMaskAnimator : MonoBehaviour
+    {
+        private PlayableDirector director;
+        private SpriteMask mask;
+
+        private void Start()
+        {
+            mask = GetComponent<SpriteMask>();
+            director = GetComponent<PlayableDirector>();
+            director.stopped += delegate { mask.enabled = false;  };
+        }
+
+        public void Play()
+        {
+            director.Play();
+            mask.enabled = true;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Play();
+            }
+        }
+    }
+}
