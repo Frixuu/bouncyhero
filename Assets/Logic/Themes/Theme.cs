7d0ee4846ea983b4d3913756d1415ef7f3514956
Frixu <frixu@frixu.me>
2019-03-24 21:36:24 +0100

    Start work on theming

---
+++ Assets/Logic/Themes/Theme.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Frixu.BouncyHero.Themes
+{
+    public class Theme
+    {
+        /// <summary> Sprite used for the background. </summary>
+        public Sprite Background { get; set; }
+        /// <summary> Color used for oncoming obstacles and their indicators. </summary>
+        public Color ObstacleColor { get; set; }
+        /// <summary> Color used for game area boundaries. </summary>
+        public Color BoundariesColor { get; set; }
+        /// <summary> Determines if the theme is unlocked
+        /// (eg. it's a reward or in-game purchase). </summary>
+        public bool Unlocked { get; set; } = true;
+        /// <summary> Name of the theme. </summary>
+        public string Name { get; set; } = "Not named";
+        public override string ToString() => $"Theme: {Name}";
+    }
+}
---
+++ Assets/Logic/Themes/ThemeManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Frixu.BouncyHero.Themes
+{
+    public class ThemeManager
+    {
+        private List<Theme> AllThemes;
+        public Theme CurrentTheme { get; private set; }
+        private Random RNG;
+
+        public ThemeManager()
+        {
+            RNG = new Random();
+            AllThemes = new List<Theme>
+            {
+                new Theme
+                {
+                    Name = "Calm Lake",
+                    Background = Resources.Load<Sprite>("CalmLake"),
+                    ObstacleColor = new Color(0.84f, 0.75f, 0.59f, 1f),
+                    BoundariesColor = new Color(0.07f, 0.1f, 0.15f, 1f)
+                },
+                new Theme
+                {
+                    Name = "Industrial",
+                    Background = Resources.Load<Sprite>("Industrial"),
+                    ObstacleColor = new Color(0.77f, 0.21f, 0.27f, 1f),
+                    BoundariesColor = new Color(0.3f, 0.14f, 0.14f, 1f)
+                }
+            };
+            CurrentTheme = RandomTheme;
+        }
+
+        private void ChooseNewTheme()
+        {
+            Theme newTheme;
+            do { newTheme = RandomTheme; } while (newTheme == CurrentTheme);
+            CurrentTheme = newTheme;
+            ThemeChanged?.Invoke(this, newTheme);
+        }
+
+        public event EventHandler<Theme> ThemeChanged; 
+
+        public Theme RandomTheme
+        {
+            get
+            {
+                var unlocked = AllThemes.Where(t => t.Unlocked).ToList();
+                return unlocked[RNG.Next(unlocked.Count)];
+            }
+        }
+    }
+}
