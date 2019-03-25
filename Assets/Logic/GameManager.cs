010ede2d451060fc0e2ad9071a08df4193284d1b
Frixu <frixu@frixu.me>
2019-03-25 16:50:11 +0100

    Create a game manager

---
+++ Assets/Logic/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Frixu.BouncyHero.Themes;
+
+namespace Frixu.BouncyHero
+{
+    public static class GameManager
+    {
+        public static ThemeManager Themes { get; }
+        private static bool gamePaused;
+
+        public static bool Paused
+        {
+            get => gamePaused;
+            set
+            {
+                gamePaused = value;
+                if (value) GamePaused?.Invoke(null, EventArgs.Empty);
+                else GameResumed?.Invoke(null, EventArgs.Empty);
+            }
+        }
+
+        public static event EventHandler GamePaused, GameResumed;
+
+        static GameManager()
+        {
+            Themes = new ThemeManager();
+            Paused = true;
+        }
+    }
+}
