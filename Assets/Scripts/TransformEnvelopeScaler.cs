4fc6dadbf6a99b7a6ef64fa40d77d18ca9756710
Frixu <frixu@frixu.me>
2019-03-26 12:40:09 +0100

    Create a component system for scaling backgrounds

---
+++ Assets/Scripts/TransformEnvelopeScaler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Transforms;
+
+namespace Frixu.BouncyHero.Scripts
+{
+    [Serializable]
+    public struct TransformEnvelopeScaler : IComponentData
+    {
+        public float Size;
+        public float TargetAspectRatio;
+    }
+
+    public class TransformEnvelopeScaleSystem : ComponentSystem
+    {
+        private struct Filter
+        {
+            public readonly int Length;
+            public ComponentDataArray<TransformEnvelopeScaler> scaleData;
+            public TransformAccessArray transforms;
+        }
+
+        [Inject] private Filter filter;
+
+        protected override void OnUpdate()
+        {
+            var screenRatio = (float) Screen.width / (float) Screen.height;
+            for (var i = 0; i < filter.Length; i++)
+            {
+                var side = filter.scaleData[i].Size;
+                if (screenRatio < filter.scaleData[i].TargetAspectRatio)
+                    side *= filter.scaleData[i].TargetAspectRatio / screenRatio;
+                filter.transforms[i].localScale = new Vector3(side, side, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TransformEnvelopeScalerProxy.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace Frixu.BouncyHero.Scripts
+{
+    public class TransformEnvelopeScalerProxy : ComponentDataProxy<TransformEnvelopeScaler>
+    {
+
+    }
+}
