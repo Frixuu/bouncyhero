6fb6e512da3fc55cd501f50a0674b362d3cfa2a2
Frixu <frixu@frixu.me>
2019-03-29 01:13:25 +0100

    Implement the TimeManager

--- Assets/Logic/Managers/TimeManager.cs
+++ Assets/Logic/Managers/TimeManager.cs
@@ -10,13 +10,29 @@ namespace Frixu.BouncyHero.Managers
 {
     public class TimeManager : ComponentSystem
     {
+        public TimeSpan CurrentTime { get; private set; }
+
         public TimeManager()
         {
+            CurrentTime = TimeSpan.Zero;
         }
 
         protected override void OnUpdate()
         {
-            
+            var delta = Time.deltaTime;
+            if (!World.Active.GetExistingManager<GameManager>().Paused)
+            {
+                CurrentTime += TimeSpan.FromSeconds(delta);
+            }
+        }
+
+        public static string TimeFormatter(TimeSpan span)
+        {
+            var time = (float)span.TotalSeconds;
+            var builder = new StringBuilder();
+            if (time >= 60f) builder.Append(Mathf.Floor(time / 60f)).Append(":");
+            builder.Append((time % 60f).ToString("00.00"));
+            return builder.ToString();
         }
     }
 }
