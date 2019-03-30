e43f792890bcd6cdf17edd7da415661394613969
Frixu <frixu@frixu.me>
2019-03-30 23:41:02 +0100

    Move camera content fitting script to a system

---
+++ Assets/ECS/Components/OrthographicEnvelope.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Frixu.BouncyHero.Components
+{
+    [Serializable]
+    public struct OrthographicEnvelope : IComponentData
+    {
+        public float Width;
+        public float Height;
+    }
+}
---
+++ Assets/ECS/Systems/CameraEnvelopeSystem.cs
@@ -0,0 +1,26 @@
+﻿using Frixu.BouncyHero.Components;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Frixu.BouncyHero.Systems
+{
+    /// <summary> Sets orthographic cameras to envelope a certain area. </summary>
+    public class CameraEnvelopeSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            var screenRatio = (float)Screen.width / (float)Screen.height;
+
+            Entities.WithAll<OrthographicEnvelope, Camera>()
+            .ForEach((ref OrthographicEnvelope data, Camera camera) =>
+            {
+                var envelopeRatio = data.Width / data.Height;
+
+                if (envelopeRatio > screenRatio)
+                    camera.orthographicSize = data.Width / screenRatio;
+                else
+                    camera.orthographicSize = data.Height;
+            });
+        }
+    }
+}
