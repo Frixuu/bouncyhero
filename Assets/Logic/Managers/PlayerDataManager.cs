0773f45318c85943d7c8238fb53a103a9c100469
Frixu <frixu@frixu.me>
2019-03-30 16:00:52 +0100

    Add a manager for game saves

---
+++ Assets/Logic/Managers/PlayerDataManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+
+namespace Frixu.BouncyHero.Managers
+{
+    public class PlayerDataManager : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/BouncyHeroBootstrap.cs
+++ Assets/Scripts/BouncyHeroBootstrap.cs
@@ -6,12 +6,14 @@ namespace Frixu.BouncyHero.Scripts
 {
     public static class BouncyHeroBootstrap
     {
+        /// <summary> Loads needed scenes and game managers. </summary>
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         private static void Bootstrap()
         {
             World.Active.GetOrCreateManager<GameManager>();
             World.Active.GetOrCreateManager<TimeManager>();
             World.Active.GetOrCreateManager<ThemeManager>();
+            World.Active.GetOrCreateManager<PlayerDataManager>();
             Debug.Log("The game has been successfully bootstrapped.");
         }
     }
