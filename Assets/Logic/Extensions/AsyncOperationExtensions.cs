2f7491b81c0333f7b8a42cf0b6e5977237f2ac97
Frixu <frixu@frixu.me>
2019-03-31 01:09:41 +0100

    Allow for awaiting AsyncOperation
    There is an issue where scenes don't load on the Android build, but in editor they work properly. I'm investigating.

---
+++ Assets/Logic/Extensions/AsyncOperationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Frixu.BouncyHero.Extensions
+{
+    /// <summary> Methods that make work with AsyncOperations easier. </summary>
+    public static class AsyncOperationExtensions
+    {
+        /// <summary>
+        /// Allows to use await keyword with AsyncOperations,
+        /// such as LoadLevelAsync.
+        /// </summary>
+        public static TaskAwaiter GetAwaiter(this AsyncOperation operation)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            operation.completed += delegate { tcs.TrySetResult(null); };
+            if (operation.isDone) tcs.TrySetResult(null);
+            return ((Task)tcs.Task).GetAwaiter();
+        }
+    }
+}
--- Assets/Scripts/BouncyHeroBootstrap.cs
+++ Assets/Scripts/BouncyHeroBootstrap.cs
@@ -1,19 +1,31 @@
-﻿using Frixu.BouncyHero.Managers;
+﻿using Frixu.BouncyHero.Extensions;
+using Frixu.BouncyHero.Managers;
 using Unity.Entities;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Frixu.BouncyHero.Scripts
 {
     public static class BouncyHeroBootstrap
     {
+        private static bool RunPreviously;
+
         /// <summary> Loads needed scenes and game managers. </summary>
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-        private static void Bootstrap()
+        private static async void Bootstrap()
         {
+            if (RunPreviously) return;
+            RunPreviously = true;
             World.Active.GetOrCreateManager<GameManager>();
             World.Active.GetOrCreateManager<TimeManager>();
             World.Active.GetOrCreateManager<ThemeManager>();
             World.Active.GetOrCreateManager<PlayerDataManager>();
+
+            await SceneManager.LoadSceneAsync("Gameplay", LoadSceneMode.Additive);
+            Debug.Log("The gameplay scene has been loaded.");
+            await SceneManager.LoadSceneAsync("UserInterface", LoadSceneMode.Additive);
+            Debug.Log("The UI scene has been loaded.");
+
             Debug.Log("The game has been successfully bootstrapped.");
         }
     }
