2bc8c6508c03441e43fdf4548a8344ac5b824ede
Frixu <frixu@frixu.me>
2019-04-09 21:55:22 +0200

    Add movable component
    Movables move automatically every frame. This component can be applied to player, obstacles, collectibles and others.

---
+++ Assets/ECS/Components/Movable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Frixu.BouncyHero.Components
+{
+    [Serializable]
+    public struct Movable : IComponentData
+    {
+        public float2 Velocity;
+    }
+}
---
+++ Assets/ECS/Systems/MoveSystem.cs
@@ -0,0 +1,47 @@
+﻿using Frixu.BouncyHero.Components;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+namespace Frixu.BouncyHero.Systems
+{
+    public class MoveSystem : JobComponentSystem
+    {
+        private struct MoveJob : IJobParallelForTransform
+        {
+            public float delta;
+
+            [DeallocateOnJobCompletion, ReadOnly]
+            public NativeArray<Movable> movables;
+
+            public void Execute(int index, TransformAccess transform)
+            {
+                transform.localPosition += new Vector3
+                (
+                    movables[index].Velocity.x * delta,
+                    movables[index].Velocity.y * delta,
+                    0f
+                );
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var query = GetEntityQuery
+            (
+                ComponentType.ReadWrite<Transform>(),
+                ComponentType.ReadOnly<Movable>()
+            );
+
+            var job = new MoveJob
+            {
+                delta = Time.deltaTime,
+                movables = query.ToComponentDataArray<Movable>(Allocator.TempJob)
+            };
+                
+            return job.Schedule(query.GetTransformAccessArray(), inputDeps);
+        }
+    }
+}
